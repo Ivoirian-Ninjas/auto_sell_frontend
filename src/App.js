1048d331896e67b33974d9d96a7a50995d8b14dc
Armel <armelfli50@gmail.com>
2020-06-02 12:08:24 -0500

    set up initial files and added react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route, Switch } from "react-router";
+import CarIndex from './views/car/CarIndex'
+import CarEdit from './views/car/CarEdit'
+import CarNew from './views/car/CarNew'
+import CarShow from './views/car/CarShow'
+import Profile from './views/user/Profile'
+import Analytics from './views/user/Analytic'
+import Favorite from './views/user/Favorite'
+import BookingCalendar from './views/user/BookingCalendar'
+import Cart from './views/cart/Cart'
+import CarPartEdit from './views/car_part/CarPartEdit'
+import CarPartIndex from './views/car_part/CarPartIndex'
+import CarPartNew from './views/car_part/CarPartIndex'
+import LogIn from './views/user/LogIn'
+import SignUp from './views/user/SignUp'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>    
+      <Switch>
+          <React.Fragment>
+            
+              {/**The intial page will be the car index*/}
+              <Route exact path='/' render={renderProps => <CarIndex {...renderProps} /> } />
+
+              {/*These routes are the routes for the cart */}
+              <Route  exact path='/cars/:id/edit' render={renderProps => <CarEdit {...renderProps}/>} />
+              <Route   exact path='/cars/new' render={renderProps => <CarNew {...renderProps} />} />            
+              <Route exact path = "/cars/:id" render={renderProps => <CarShow {...renderProps}/>} />
+
+
+              {/* user routes */}
+              <Route path='/users/:id' render={renderProps => <Profile/>} />
+              <Route path='/users/:id/favorites' render={renderProps => <Favorite/>} />
+              {/*These routes are the routes for the booking process */}          
+              <Route exact path='/bookings' render={renderProps => <BookingCalendar {...renderProps}/>}  />
+              {/**Display analytics for the admin only */}
+              <Route path='/analytics' render={renderProps => <Analytics/>} />
+              <Route exact path='/login' render={renderProps => <LogIn {...renderProps}/>}  />
+              <Route exact path='/signup' render={renderProps => <SignUp {...renderProps}/>}  />
+
+
+              {/**Cart routes */}
+              <Route path='/cart' render={renderProps => <Cart/>} />
+
+              {/**Car Parts routes */}
+              <Route path='/parts/new' render={renderProps => <CarPartNew/>} />
+              <Route path='/parts' render={renderProps => <CarPartIndex/>} />
+              <Route path='/parts/:id/edit' render={renderProps => <CarPartEdit/>} />
+
+              
+            </React.Fragment>
+         
+       </Switch>
+    </Router>
   );
 }
 
