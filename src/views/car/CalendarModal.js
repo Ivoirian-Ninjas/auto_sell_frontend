bf02125cf56e1aebcd4c76e8f27801fa1e9fa63d
Ivoirian-Ninjas <michelkonan00@gmail.com>
2020-06-17 08:00:11 +0000

    show page style done

--- src/views/car/CalendarModal.js
+++ src/views/car/CalendarModal.js
@@ -2,25 +2,19 @@ import React from 'react'
 import Iframe from 'react-iframe'
 
 
-export default function CalendarModal() {
+export default function CalendarModal(props) {
     return (
-        <div style={{display:'flex', width: '60%', "max-width": '100%', margin: '0 auto',
-        position: 'fixed',
-        left: '50%',
-        top: '50%',
-        transform: 'translate(-50%, -50%)',
-        "z-index": 29999,
-        "background-color": '#fff',
-        "flex-direction": 'column',
-        "box-shadow": '0px 0px 0px 400px rgba(0, 0, 0, 0.40)'}}>
+        <div className="modal_schedule">
+        <button onClick={props.close_modal} className="close_modal">
+            x 
+        </button>
 
 
-
-        <h1>Let's Schedule</h1>
+        <h1 className="let_s">Let's Schedule</h1>
         {/* <iframe  style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe> */}
         <Iframe url="https://calendly.com/hilaire-auto-sell/60min"
-        width="600px"
-        height="450px"
+        width="100%"
+        height="500px"
         id="myId"
         className="myClassname"
         display="initial"
